37fd2770065f1953e93bd53c8efd87a2b23533cf
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-28 01:27:55 -0300

    Updated calculator style

--- src/screens/Help/index.js
+++ src/screens/Help/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { ScrollView, StyleSheet, View, Text as RNText, TouchableOpacity, TextInput, Modal } from 'react-native';
 
 function Text(props) {
-    return <RNText {...props} style={[props.style, {}]} />;
+    return <RNText {...props} style={[props.style, { fontFamily: 'Poppins_600SemiBold' }]} />;
 }
 
 export default function FinanceScreen() {
@@ -85,156 +85,167 @@ export default function FinanceScreen() {
                 </TouchableOpacity>
             </View>
             <Modal visible={showCalculator} transparent={true}>
-                <View style={styles.calculatorContainer}>
-                    <TextInput
-                        style={styles.calculatorInput}
-                        value={input}
-                        editable={false}
-                    />
-                    <View style={styles.buttonsContainer}>
-                        {["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "+", "-", "*", "/", "="].map((button, index) => (
+                <View style={styles.calculatorOverlay}>
+                    <View style={styles.calculatorContainer}>
+                        <TextInput
+                            style={styles.calculatorInput}
+                            value={`R$ ${input}`}
+                            editable={false}
+                            textAlign={'left'}
+                        />
+                        <View style={styles.buttonsContainer}>
+                            {["C", "±", "%", "/", "7", "8", "9", "*", "4", "5", "6", "+", "1", "2", "3", "-", ",", "0", "D"].map((button, index) => (
+                                <TouchableOpacity
+                                    key={index}
+                                    style={[styles.button, button.match(/[0-9,]/) ? styles.numberButton : styles.operatorButton]}
+                                    onPress={() => {
+                                        if (button === "R") {
+                                            setShowCalculator(false);
+                                        } else if (button === "D") {
+                                            handleCalculate();
+                                        } else {
+                                            handlePress(button);
+                                        }
+                                    }}
+                                >
+                                    <Text style={styles.buttonText}>{button}</Text>
+                                </TouchableOpacity>
+                            ))}
                             <TouchableOpacity
-                                key={index}
-                                style={styles.button}
-                                onPress={() => {
-                                    if (button === "=") {
-                                        handleCalculate();
-                                    } else {
-                                        handlePress(button);
-                                    }
-                                }}
+                                style={[styles.button, styles.confirmButton]}
+                                onPress={() => setShowCalculator(false)}
                             >
-                                <Text style={styles.buttonText}>{button}</Text>
+                                <Text style={styles.buttonText}>R</Text>
                             </TouchableOpacity>
-                        ))}
-                        <TouchableOpacity style={styles.button} onPress={handleClear}>
-                            <Text style={styles.buttonText}>C</Text>
-                        </TouchableOpacity>
+                        </View>
                     </View>
-                    <TouchableOpacity style={styles.doneButton} onPress={() => setShowCalculator(false)}>
-                        <Text style={styles.doneButtonText}>Concluir</Text>
-                    </TouchableOpacity>
                 </View>
             </Modal>
+
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    summaryContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 28
-    },
-    summaryCard: {
-        flex: 0.48,
-        padding: 15,
-        borderRadius: 12,
-        borderWidth: 2
-    },
-    summaryText: {
-        fontSize: 14,
-        fontFamily: 'Poppins_600SemiBold'
-    },
-    summaryAmount: {
-        fontSize: 18,
-        fontFamily: 'Poppins_700Bold'
-    },
-    recordCard: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        borderWidth: 1,
-        borderColor: 'rgba(0, 0, 0, 0.1)',
-        borderRadius: 12,
-        padding: 15,
-        marginBottom: 20
-    },
-    recordCategory: {
-        fontSize: 14,
-        fontFamily: 'Poppins_600SemiBold'
-    },
-    recordAmount: {
-        fontSize: 12,
-        fontFamily: 'Poppins_400Regular'
-    },
-    fabContainer: {
-        position: 'absolute',
-        bottom: 28,
-        right: 28,
-    },
-    fabMainButton: {
-        width: 56,
-        height: 56,
-        borderRadius: 28,
-        backgroundColor: '#FB5F21',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 12,
-    },
-    fabButton: {
-        width: 56,
-        height: 56,
-        borderRadius: 28,
-        backgroundColor: '#FB5F21',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 12,
-    },
-    fabText: {
-        fontSize: 24,
-        color: 'white',
-    },
-    calculatorContainer: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'rgba(0,0,0,0.7)',
-    },
-    calculatorInput: {
-      width: '80%',
+  summaryContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 28,
+  },
+  summaryCard: {
+      flex: 0.48,
       padding: 15,
-      fontSize: 24,
-      textAlign: 'right',
-      marginBottom: 20,
-      backgroundColor: 'white',
-      borderRadius: 8,
+      borderRadius: 12,
+      borderWidth: 2,
+  },
+  summaryText: {
+      fontSize: 14,
+      fontFamily: 'Poppins_600SemiBold',
+  },
+  summaryAmount: {
+      fontSize: 18,
+      fontFamily: 'Poppins_700Bold',
+  },
+  recordCard: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
       borderWidth: 1,
       borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    buttonsContainer: {
+      borderRadius: 12,
+      padding: 15,
+      marginBottom: 20,
+  },
+  recordCategory: {
+      fontSize: 14,
+      fontFamily: 'Poppins_600SemiBold',
+  },
+  recordAmount: {
+      fontSize: 12,
+      fontFamily: 'Poppins_400Regular',
+  },
+  fabContainer: {
+      position: 'absolute',
+      bottom: 28,
+      right: 28,
+  },
+  fabMainButton: {
+      width: 56,
+      height: 56,
+      borderRadius: 28,
+      backgroundColor: '#FB5F21',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 12,
+  },
+  fabButton: {
+      width: 56,
+      height: 56,
+      borderRadius: 28,
+      backgroundColor: '#FB5F21',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 12,
+  },
+  fabText: {
+      fontSize: 24,
+      color: 'white',
+      fontFamily: 'Poppins_600SemiBold',
+  },
+  calculatorOverlay: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+  calculatorContainer: {
+      backgroundColor: 'white',
+      borderTopLeftRadius: 16,
+      borderTopRightRadius: 16,
+      padding: 16,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: -2 },
+      shadowOpacity: 0.3,
+      shadowRadius: 4,
+  },
+  calculatorInput: {
+      width: '100%',
+      padding: 15,
+      fontSize: 24,
+      textAlign: 'left',
+      marginBottom: 20,
+      backgroundColor: 'white',
+      borderWidth: 0,  // Removed the border
+  },
+  buttonsContainer: {
       flexDirection: 'row',
       flexWrap: 'wrap',
       justifyContent: 'space-between',
-      width: '80%',
-    },
-    button: {
-      width: '23%',
-      padding: 20,
-      marginBottom: 10,
+      margin: 1,  // Added spacing
+  },
+  button: {
+      width: '24%',
+      padding: 16,
       alignItems: 'center',
       justifyContent: 'center',
+      borderRadius: 8,  // Rounded corners
+      margin: 1,  // Added spacing
+  },
+  numberButton: {
       backgroundColor: 'white',
-      borderRadius: 8,
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.1,  // Light shadow
+      shadowRadius: 1,  // Light shadow
+  },
+  operatorButton: {
+      borderColor: 'rgba(0, 0, 0, 0.2)',
       borderWidth: 1,
-      borderColor: 'rgba(0, 0, 0, 0.1)',
-    },
-    buttonText: {
+  },
+  confirmButton: {
+      backgroundColor: '#FB5F21',  // Confirm button color
+  },
+  buttonText: {
       fontSize: 24,
       color: 'black',
-    },
-    doneButton: {
-      backgroundColor: '#FB5F21',
-      borderRadius: 8,
-      padding: 10,
-      marginTop: 20,
-    },
-    doneButtonText: {
-      color: 'white',
-      fontSize: 16,
-      textAlign: 'center',
-    },
-  calculatorButtons: {
-      flexDirection: 'row',
-      marginBottom: 20,
+      fontFamily: 'Poppins_600SemiBold',
   },
 });
