572f1b13e3041f8c5a8ad05f900ee582595d3b66
Augusto Balsanelli <github.891d8@passfwd.com>
2023-08-27 23:12:05 -0300

    Created initial idea for money manager

--- src/screens/Help/index.js
+++ src/screens/Help/index.js
@@ -1,181 +1,112 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, StyleSheet, View, Text as RNText, TouchableOpacity, Image, Animated, Modal, Button, Linking, TextInput } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useFonts, Poppins_400Regular, Poppins_600SemiBold, Poppins_700Bold } from '@expo-google-fonts/poppins';
-import { createStackNavigator } from '@react-navigation/stack';
-import DateTimePickerModal from 'react-native-modal-datetime-picker';
-
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
+import { ScrollView, StyleSheet, View, Text as RNText, TouchableOpacity } from 'react-native';
 
 function Text(props) {
     return <RNText {...props} style={[props.style, {}]} />;
-  }
+}
 
-  
-  function HelpScreen({ navigation }) {
-    const questions = [
-      {
-        id: 1,
-        question: 'Como faço para manter minha esteira em boas condições?',
-        answer: 'bla bla bla...'
-      },
-      {
-        id: 2,
-        question: 'Qual é a melhor maneira de limpar minha máquina de academia?',
-        answer: 'ohhhh aaaaaaaa'
-      },
-    ];
-  
-    const Separator = () => <View style={{ width: '100%', height: 1, backgroundColor: 'rgba(0, 0, 0, 0.1)', marginVertical: 20 }} />;
-  
-    return (
-      <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: 'white', padding: 28 }}>
-        {questions.map(({ id, question, answer }) => (
-          <View key={id}>
-            <Separator />
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('QuestionScreen', { question, answer })}>
-              <Text style={styles.buttonTitle}>{question}</Text>
-              <Image
-                source={{ uri: 'https://cdn.discordapp.com/attachments/1059425565330911284/1131681200059207740/right-arrow_1.png' }}
-                style={{ marginLeft: 'auto', width: 20, height: 20 }}
-              />
-            </TouchableOpacity>
-          </View>
-        ))}
-        <Separator />
-      </ScrollView>
-    );
-  }
-  
-  function QuestionScreen({ route }) {
-    const { question, answer } = route.params;
-  
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', padding: 28 }}>
-        <Text style={{ fontSize: 20, marginBottom: 20, fontFamily: 'Poppins_600SemiBold', textAlign: 'center' }}>{question}</Text>
-        <Text style={{ fontFamily: 'Poppins_400Regular' }}>{answer}</Text>
-      </View>
-    );
-  }
+export default function FinanceScreen() {
+    const categories = ['Incomes', 'Expenses'];
+    const records = Array(30).fill().map((_, i) => ({
+        id: i,
+        category: categories[i % categories.length],
+        amount: (i * 100).toFixed(2),
+        date: `2023-${String(i % 12 + 1).padStart(2, '0')}-01`
+    })).sort((a, b) => a.date.localeCompare(b.date));
 
-export default function HelpStack() {
-    return (
-    <Stack.Navigator>
-        <Stack.Screen
-        name="Ajuda"
-        component={HelpScreen}
-        options={{ headerShown: false }}
-        />
-        <Stack.Screen
-        name="QuestionScreen"
-        component={QuestionScreen}
-        options={{ headerShown: false }}
-        />
+    let lastMonth = null;
 
-    </Stack.Navigator>
+    const totalIncomes = records.filter(r => r.category === 'Incomes').reduce((sum, r) => sum + parseFloat(r.amount), 0);
+    const totalExpenses = records.filter(r => r.category === 'Expenses').reduce((sum, r) => sum + parseFloat(r.amount), 0);
+
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={styles.summaryContainer}>
+                <View style={[styles.summaryCard, { backgroundColor: '#FB5F21' }]}>
+                    <Text style={styles.summaryText}>Total Incomes</Text>
+                    <Text style={styles.summaryAmount}>$ {totalIncomes.toFixed(2)}</Text>
+                </View>
+                <View style={[styles.summaryCard, { backgroundColor: 'gray' }]}>
+                    <Text style={styles.summaryText}>Total Expenses</Text>
+                    <Text style={styles.summaryAmount}>$ {totalExpenses.toFixed(2)}</Text>
+                </View>
+            </View>
+            <ScrollView contentContainerStyle={{ flexGrow: 1, padding: 28 }}>
+                {records.map((record) => {
+                    const currentMonth = new Date(record.date).getMonth();
+                    const shouldDisplayMonth = lastMonth !== currentMonth;
+                    lastMonth = currentMonth;
+
+                    return (
+                        <View key={record.id}>
+                            {shouldDisplayMonth && (
+                                <View style={styles.monthSeparator}>
+                                    <Text style={styles.monthText}>{new Date(record.date).toLocaleString('default', { month: 'long' })}</Text>
+                                </View>
+                            )}
+                            <TouchableOpacity style={styles.recordCard}>
+                                <View style={styles.recordContent}>
+                                    <Text style={styles.recordCategory}>{record.category}</Text>
+                                    <Text style={styles.recordAmount}>$ {record.amount}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    );
+                })}
+            </ScrollView>
+        </View>
     );
 }
 
-
 const styles = StyleSheet.create({
-    button: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      width: '100%',
+    summaryContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 28
     },
-    buttonTitle: {
-      fontFamily: "Poppins_600SemiBold",
-      fontSize: 15,
+    summaryCard: {
+        flex: 0.48,
+        padding: 15,
+        borderRadius: 12
     },
-    buttonText: {
-      fontFamily: "Poppins_400Regular",
-      fontSize: 12,
-      color: 'rgba(0, 0, 0, 0.6)',
+    summaryText: {
+        fontSize: 14,
+        fontFamily: 'Poppins_600SemiBold'
     },
-  
-    // Pieces
-    modalContainer: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      backgroundColor: 'rgba(0,0,0,0.5)',
+    summaryAmount: {
+        fontSize: 18,
+        fontFamily: 'Poppins_700Bold'
     },
-    modalContent: {
-      backgroundColor: 'white',
-      padding: 20,
-      borderTopLeftRadius: 10,
-      borderTopRightRadius: 10,
+    recordCard: {
+        width: '100%',
+        borderWidth: 1,
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        borderRadius: 12,
+        padding: 15,
+        marginBottom: 20
     },
-    modalHeader: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
+    recordCategory: {
+        fontSize: 14,
+        fontFamily: 'Poppins_600SemiBold'
     },
-    modalTitle: {
-      fontSize: 18,
-      fontFamily: 'Poppins_600SemiBold',
+    recordAmount: {
+        fontSize: 12,
+        fontFamily: 'Poppins_400Regular'
     },
-    modalSubtitle: {
-      fontSize: 14,
-      fontFamily: 'Poppins_400Regular',
-      marginTop: 10,
+    monthSeparator: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+        marginTop: 20
     },
-    filterOptions: {
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      justifyContent: 'space-between',
+    monthText: {
+        fontFamily: 'Poppins_400Regular',
+        fontSize: 12,
+        marginRight: 10
     },
-    filterButton: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderWidth: 1,
-      borderColor: '#FB5F21',
-      borderRadius: 50,
-      marginVertical: 5,
-      padding: 10,
-      backgroundColor: 'white',
-      width: '48%',
-    },
-    filterButtonActive: {
-      backgroundColor: '#FB5F21',
-    },
-    filterText: {
-      color: '#000',
-      fontFamily: 'Poppins_400Regular',
-    },
-    filterTextActive: {
-      color: '#fff',
-      fontFamily: 'Poppins_400Regular',
-    },
-    filterActions: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginTop: 10,
-    },
-    clearFilterButton: {
-      backgroundColor: 'white',
-      borderRadius: 50,
-      padding: 10,
-      borderWidth: 1,
-      borderColor: '#FB5F21',
-      width: '48%',
-    },
-    clearFilterText: {
-      color: '#FB5F21',
-      textAlign: 'center',
-      fontFamily: 'Poppins_400Regular',
-    },
-    doneButton: {
-      backgroundColor: '#FB5F21',
-      borderRadius: 50,
-      padding: 10,
-      width: '48%',
-    },
-    doneButtonText: {
-      color: '#fff',
-      textAlign: 'center',
-      fontFamily: 'Poppins_400Regular',
-    },
-  });
+    recordContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+});
